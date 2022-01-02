a39c1748b0cede19e669905d7c572e20fdc23b0a
jishu-yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-01-02 10:12:29 +0530

    GS q3

---
+++ q3.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        long long int start=0, end=0, product=1, count=0;
+        while(end< n){
+            product*=a[end];
+            while(start< n&& product>=k){
+                product/=a[start];
+                start++;
+            }
+            if(product < k){
+                count += end-start+1;
+            }
+            end++;
+        }
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        long long int k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.countSubArrayProductLessThanK(arr, n, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
